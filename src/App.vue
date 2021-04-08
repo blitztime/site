8ce02386c8beb77f4d71159c7079728dd127055c
Artemis21 <artemisdev21@gmail.com>
2021-04-08 17:30:38 +0100

    Add home page.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,24 @@
 <template lang="pug">
-router-view
+div.root
+    router-view
+    footer.footer
+        | © Copyright 2021 !{' '}
+        a(href='https://artemisdev.xyz') Artemis
+        | .
 </template>
+
+<style lang="sass">
+@import 'sass/_variables'
+
+body
+    font-family: $font
+    color: $theme-colour
+    background: $bg-colour
+    margin: 0
+
+.footer
+    background: #111
+    padding: 0.5rem
+    text-align: center
+    height: 1.2rem
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,88 @@
 <template lang="pug">
-main
-    | Welcome to Blitz.Red!
+main.intro
+    img.logo(src='/img/logo.svg')
+    h1.header Blitz.Red
+    h2.subheader The live online game timer for competitive 1v1s
+    router-link.button(to='/create') Create a Timer
+    p.status
+        | {{ stats.ongoingTimers }} ongoing timers &bull;
+        | {{ stats.allTimers }} all time &bull;
+        | {{ stats.connected }} people online
 </template>
 
 <script>
+import { HttpClient } from 'blitztime';
+
+const client = new HttpClient('https://api.blitz.red');
+
 export default {
     name: 'Home',
+    data() {
+        return {
+            stats: {
+                ongoingTimers: '?',
+                allTimers: '?',
+                connected: '?',
+            },
+        };
+    },
+    created() {
+        client.getStats().then((stats) => {
+            this.stats = stats;
+        });
+    },
 };
 </script>
 
 <style lang="sass" scoped>
-main
-    color: red
+@import '../sass/_variables'
+
+.intro
+    display: flex
+    flex-direction: column
+    align-items: center
+    justify-content: center
+    min-height: calc(100vh - 2.2rem)
+
+.logo
+    width: 10rem
+
+.header
+    font-weight: 400
+    margin: 0.2rem
+
+.subheader
+    font-weight: 300
+    margin: 0.2rem
+    text-align: center
+    &:after
+        content: '_'
+        display: inline-block
+        width: 1ch
+        animation: cursor_blink 1s linear 0s infinite
+
+@keyframes cursor_blink
+    0%
+        content: '_'
+    60%
+        content: ' '
+
+.button
+    background: $theme-colour
+    color: $bg-colour
+    padding: 5px 10px
+    transition: padding 50ms
+    margin: 0.5rem
+    &:after
+        content: ' →'
+        margin-left: -5px
+        margin-right: 5px
+        transition: margin 50ms
+    &:hover:after
+        margin: 0
+
+.status
+    margin: 0.5rem
+    font-weight: 400
+    text-align: center
 </style>
