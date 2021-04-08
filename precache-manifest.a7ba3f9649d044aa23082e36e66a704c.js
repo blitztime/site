b8df6013dd3ac646dfae2ebc96dbec1854e8b57c
Artemis21 <hollyshort21@gmail.com>
2021-04-08 14:07:58 +0000

    Deploying to build from @ blitztime/site@0935b6fc4385c5d90ea2cd808641be9f1fbf824f 🚀

---
+++ precache-manifest.a7ba3f9649d044aa23082e36e66a704c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d768e776216213456002d53a28a2bbde",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "a6bba50f5164ef24f6a6",
+    "url": "/css/app.c11d702c.css"
+  },
+  {
+    "revision": "1fba462f42d9ea6b0e0fc7029da00bd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6bba50f5164ef24f6a6",
+    "url": "/js/app.e1b9466a.js"
+  },
+  {
+    "revision": "f28e1fe54cbb3ca988a4",
+    "url": "/js/chunk-vendors.7e47f066.js"
+  },
+  {
+    "revision": "8ac85b9a75c04d6a69db1aeb18fd2360",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
